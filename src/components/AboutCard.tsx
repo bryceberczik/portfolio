68701bd1c9d8be8c8be8488b978db1725050b28b
berczikbryce <bryceberczik.dev@gmail.com>
2025-02-05 15:06:14 -0600

    added about me cards

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,29 +1,32 @@
-import { FiCloudLightning } from "react-icons/fi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
+import { IAbout } from "../interfaces/interfaces";
 
-const AboutCard = () => {
+const AboutCard = ({ heading, description, icon }: IAbout) => {
   return (
     <div className="px-4 py-12">
-      <GlassmorphicCard />
+      <GlassmorphicCard
+        heading={heading}
+        description={description}
+        icon={icon}
+      />
     </div>
   );
 };
 
-const GlassmorphicCard = () => {
+const GlassmorphicCard = ({ heading, description, icon }: IAbout) => {
   return (
     <div className="group relative mx-auto w-full max-w-sm overflow-hidden rounded-lg p-0.5 transition-all duration-500 hover:scale-[1.01]">
-      <div className="relative z-10 flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-white/10 backdrop-blur-lg p-8 shadow-lg transition-colors duration-500 group-hover:bg-white/20">
-        <FiCloudLightning className="relative z-10 mb-10 mt-2 rounded-full border-2 border-white bg-white/20 p-4 text-7xl text-white backdrop-blur-md" />
+      <div className="relative custom-z flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-white/10 backdrop-blur-lg p-8 shadow-lg transition-colors duration-500 group-hover:bg-white/20">
+        <FontAwesomeIcon
+          icon={icon}
+          className="relative z-10 mb-10 mt-2 flex h-6 w-6 items-center justify-center rounded-full border-2 border-white bg-white/20 p-4 text-2xl text-white backdrop-blur-md"
+        />
 
         <h4 className="relative z-10 mb-4 w-full text-3xl font-bold text-slate-50">
-          Go faster
+          {heading}
         </h4>
-        <p className="relative z-10 text-slate-200">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ipsum
-          sed blanditiis iste molestias nemo nobis repellendus nisi dolorum
-          itaque optio impedit cum voluptatem facilis minima, quasi laborum.
-          Nihil, quaerat.
-        </p>
+        <p className="relative z-10 text-slate-200">{description}</p>
       </div>
 
       <motion.div
@@ -35,7 +38,7 @@ const GlassmorphicCard = () => {
           duration: 3.5,
           ease: "linear",
         }}
-        className="absolute inset-0 z-0 bg-gradient-to-br from-indigo-200 via-indigo-200/0 to-indigo-200 opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+        className="absolute inset-0 z-0 bg-gradient-to-br from-[#3c1e5b] via-indigo-200/0 to-[#004d42] opacity-0 transition-opacity duration-500 group-hover:opacity-100"
       />
     </div>
   );
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,5 @@
+export interface IAbout {
+    heading: string;
+    description: string;
+    icon: any;
+}
