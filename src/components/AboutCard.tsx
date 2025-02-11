f97c633bc5fab5ae0a98ebac8aca66ed09393348
berczikbryce <bryceberczik.dev@gmail.com>
2025-02-11 14:19:24 -0600

    adjusted styling on about card

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,5 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { motion } from "framer-motion";
 import { IAbout } from "../interfaces/interfaces";
 
 const AboutCard = ({ heading, description, icon }: IAbout) => {
@@ -28,18 +27,6 @@ const GlassmorphicCard = ({ heading, description, icon }: IAbout) => {
         </h4>
         <p className="relative z-10 text-slate-200">{description}</p>
       </div>
-
-      <motion.div
-        initial={{ rotate: "0deg" }}
-        animate={{ rotate: "360deg" }}
-        style={{ scale: 1.75 }}
-        transition={{
-          repeat: Infinity,
-          duration: 3.5,
-          ease: "linear",
-        }}
-        className="absolute inset-0 z-0 bg-gradient-to-br from-[#3c1e5b] via-indigo-200/0 to-[#004d42] opacity-0 transition-opacity duration-500 group-hover:opacity-100"
-      />
     </div>
   );
 };
