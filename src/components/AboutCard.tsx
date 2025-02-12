12dbbeece0189ee1e462b4123e76d04201358e67
berczikbryce <bryceberczik.dev@gmail.com>
2025-02-12 11:50:50 -0600

    first attempt at fixing error for about section

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,13 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IAbout } from "../interfaces/interfaces";
+import {
+  faUser,
+  faHeart,
+  faLightbulb,
+} from "@fortawesome/free-regular-svg-icons";
 
-const AboutCard = ({ heading, description, icon }: IAbout) => {
+const AboutCard = () => {
   return (
-    <div className="px-4 xl:px-3 py-12 2xl:px-10">
+    <div className="px-4 xl:px-3 py-12 2xl:px-40 xl:flex xl:flex-row xl:justify-center">
       <GlassmorphicCard
-        heading={heading}
-        description={description}
-        icon={icon}
+        icon={faUser}
+        heading="About Me"
+        description="Junior full-stack engineer passionate about turning ideas into code. Specializing in React, TypeScript, and Tailwind on the frontend, with Node.js and Prisma + PostgreSQL on the backend. Also proficient in the MERN stack and other technologies."
+      />
+      <GlassmorphicCard
+        icon={faHeart}
+        heading="Education"
+        description="I completed full-stack development Coding Bootcamp at UTSA, focusing on React and Node.js. During the program, I built dynamic web apps and gained hands-on experience with modern frameworks and backend development."
+      />
+      <GlassmorphicCard
+        icon={faLightbulb}
+        heading="Problem Solving"
+        description="I thrive on problem-solving, turning complex issues into efficient solutions. Whether it's debugging, optimizing performance, or designing scalable architectures, I enjoy tackling challenges and creating impactful solutions while continuously learning."
       />
     </div>
   );
@@ -15,7 +30,7 @@ const AboutCard = ({ heading, description, icon }: IAbout) => {
 
 const GlassmorphicCard = ({ heading, description, icon }: IAbout) => {
   return (
-    <div className="group relative mx-auto w-full max-w-sm overflow-hidden rounded-lg p-0.5 transition-all duration-500 hover:scale-[1.01]">
+    <div className="group relative mx-auto w-full max-w-sm overflow-hidden rounded-lg py-5 transition-all duration-500 hover:scale-[1.01]">
       <div className="relative custom-z flex flex-col items-center justify-center overflow-hidden rounded-[7px] bg-white/10 backdrop-blur-lg p-8 shadow-lg transition-colors duration-500 group-hover:bg-white/20">
         <FontAwesomeIcon
           icon={icon}
